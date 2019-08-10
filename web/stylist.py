4c4677199fd518e4a2d93db2f7247e55f790853c
Brady Sullivan <brady@bsull.com>
2019-08-10 15:35:54 -0700

    Slight improvement on stylist.py

--- web/stylist.py
+++ web/stylist.py
@@ -1,7 +1,16 @@
 import re
 
 with open("/home/brady/Downloads/stylist.html") as f:
+    items = []
     for line in f:
         if re.search("div id=\"n", line):
             line = line.strip()
-            print(line)
+            pieces = line.split(" ")
+            n = pieces[1]
+            color = pieces[3]
+            a, b = color[5], color[6]
+            h = a + b
+            item = [n, h]
+            items.append(item)
+    items.sort(key=lambda tup: tup[0])
+    print(items)
